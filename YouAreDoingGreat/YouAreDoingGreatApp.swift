c7b91ce72df9e6de43db936e40e395a6fe72e2bd
nemish <nemish.i.nelos@gmail.com>
2025-11-17 23:20:15 +0200

    Initial Commit

---
+++ YouAreDoingGreat/YouAreDoingGreatApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreDoingGreatApp.swift
+//  YouAreDoingGreat
+//
+//  Created by Ярослав Мельничук on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreDoingGreatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
